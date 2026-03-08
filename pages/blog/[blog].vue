9df537db6aefa3532c357e0698c3bf84f3a7d284
Rk-06 <land.rk06@gmail.com>
2026-03-08 15:15:41 +0900

    リストのインデントを追加/画像のトリミング/画像の最大高さの設定

--- pages/blog/[blog].vue
+++ pages/blog/[blog].vue
@@ -1,10 +1,10 @@
 <template>
     <main class="flex flex-col gap-8">
         <Header/>
-        <div class="max-w-2xl mx-auto pt-8 px-8">
+        <div class="max-w-2xl mx-auto pt-24 px-8">
             <div>
                 <h1 class="text-4xl md:text-6xl font-semibold">{{ title ?? "記事が見つかりません" }}</h1>
-                <p v-if="!!author">by: {{ author }}</p>
+                <p class="mt-2" v-if="!!author">by: {{ author }}</p>
             </div>
             <ContentDoc
                 id="document"
@@ -31,6 +31,8 @@ const { title, author } = data.value ?? {} as { [key: string]: string }
 }
 #document > h2 {
     font-size: 24px;
+    font-weight: bold;
+    margin-top: 32px;
 }
 #document > blockquote {
     padding: 8px 12px;
@@ -39,15 +41,36 @@ const { title, author } = data.value ?? {} as { [key: string]: string }
     border-left: solid 2px #cbd5e1;
     border-radius: 4px;
 }
-#document > ul {
-    list-style: disc;
+#document ul,
+#document ol {
     margin: 8px 0;
+    padding-left: 1.25rem; /* ルートのインデント */
+    list-style-position: outside;
 }
-#document > ol {
-    list-style: decimal;
-    margin: 8px 0;
+
+#document ul {
+    list-style-type: disc;
+}
+
+#document ol {
+    list-style-type: decimal;
+}
+
+#document li {
+    margin: 4px 0;
+}
+
+/* ネストされたリストはさらに内側へ */
+#document ul ul,
+#document ol ol,
+#document ul ol,
+#document ol ul {
+    margin-top: 4px;
+    padding-left: 1rem;
 }
 #document > * > img {
+    margin: 16px 0;
     border-radius: 8px;
+    max-height: 500px;
 }
 </style>
